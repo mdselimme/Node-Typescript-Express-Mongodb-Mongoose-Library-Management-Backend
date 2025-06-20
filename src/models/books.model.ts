5c254dd9e083e1061fb5dc02f0540e41aa1e1ba5
mdselimme <mdselimdev@gmail.com>
2025-06-20 20:27:13 +0600

    add books mongoose schema model

---
+++ src/models/books.model.ts
@@ -0,0 +1,45 @@
+import { Schema } from "mongoose";
+import { IBookModel } from "../interfaces/books.interface";
+
+// Books Schema Model For Validation Object Model 
+export const bookSchemaModel = new Schema<IBookModel>({
+    title: {
+        type: String,
+        required: true,
+        trim: true,
+        minlength: [3, "Book name needed min 3 character"]
+    },
+    author: {
+        type: String,
+        required: true,
+        trim: true,
+        minlength: [3, "Author name needed min 3 character"]
+    },
+    genre: {
+        type: String,
+        required: true,
+        trim: true,
+        uppercase: true,
+        enum: ["FICTION", "NON_FICTION", "SCIENCE", "HISTORY", "BIOGRAPHY", "FANTASY"]
+    },
+    isbn: {
+        type: String,
+        required: true,
+        trim: true,
+        unique: true
+    },
+    description: {
+        type: String,
+        trim: true,
+    },
+    copies: {
+        type: Number,
+        required: true
+    },
+    available: {
+        type: Boolean,
+        required: true,
+        default: true
+    }
+});
+
